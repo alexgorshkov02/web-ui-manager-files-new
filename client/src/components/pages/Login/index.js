2c0a14d00fcf5c832461d77cd82a2aab8b13d075
Alexander Gorshkov <alexgorshkov0290@gmail.com>
2023-08-18 22:48:48 -0400

    Added a login page and logic to store an auth token in the localStorage

--- client/src/components/pages/Login/index.js
+++ client/src/components/pages/Login/index.js
@@ -1,15 +1,66 @@
 import { gql, useMutation } from "@apollo/client";
+import React, { useState } from "react";
 
-export const LOGIN = gql`
-    mutation login (
-        username: String!
-        pasword: String!
-      ): Auth
-    }
+const LOGIN = gql`
+mutation Login($username: String!, $password: String!) {
+  login(username: $username, password: $password) {
+    token
+  }
+}
+`;
 
-    mutation addUser (
-        username: String!
-        pasword: String!
-      ): Auth
+const SIGNUP = gql`
+mutation AddUser($username: String!, $password: String!) {
+  addUser(username: $username, password: $password) {
+    token
+  }
+}
+`;
+
+const LoginForm = ({ changeLoginState }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, { loading, error }] = useMutation(LOGIN);
+
+  if (loading) return "Loading...";
+  if (error) {
+    // Handle any errors that occurred during the query
+    console.error(error);
+    return <div>{error.message}</div>;
+  }
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await login({
+        variables: { username: username, password: password },
+      });
+  
+      if (data.login.token) {
+        localStorage.setItem("jwt", data.login.token);
+        changeLoginState(true);
+      }
+    } catch (error) {
+      console.error("Error logging in:", error);
     }
-`
+  };
+  return (
+    <div className="login">
+      <form onSubmit={onSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
--- client/src/elements/MenuListElement/index.js
+++ client/src/elements/MenuListElement/index.js
@@ -1,18 +1,17 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import ClickAwayListener from '@mui/material/ClickAwayListener';
-import Grow from '@mui/material/Grow';
-import Paper from '@mui/material/Paper';
-import Popper from '@mui/material/Popper';
-import MenuItem from '@mui/material/MenuItem';
-import MenuList from '@mui/material/MenuList';
-import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import Grow from "@mui/material/Grow";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
+import MenuItem from "@mui/material/MenuItem";
+import MenuList from "@mui/material/MenuList";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
 
 const CustomizedButton = styled(Button)`
-color: #ffffff;
+  color: #ffffff;
 `;
-
 
 export default function MenuListComposition() {
   const [open, setOpen] = React.useState(false);
@@ -30,11 +29,16 @@ export default function MenuListComposition() {
     setOpen(false);
   };
 
+  //Add an auth check to log out
+  const logout = () => {
+    localStorage.removeItem("jwt");
+  };
+
   function handleListKeyDown(event) {
-    if (event.key === 'Tab') {
+    if (event.key === "Tab") {
       event.preventDefault();
       setOpen(false);
-    } else if (event.key === 'Escape') {
+    } else if (event.key === "Escape") {
       setOpen(false);
     }
   }
@@ -55,8 +59,8 @@ export default function MenuListComposition() {
         <CustomizedButton
           ref={anchorRef}
           id="composition-button"
-          aria-controls={open ? 'composition-menu' : undefined}
-          aria-expanded={open ? 'true' : undefined}
+          aria-controls={open ? "composition-menu" : undefined}
+          aria-expanded={open ? "true" : undefined}
           aria-haspopup="true"
           onClick={handleToggle}
         >
@@ -75,7 +79,7 @@ export default function MenuListComposition() {
               {...TransitionProps}
               style={{
                 transformOrigin:
-                  placement === 'bottom-start' ? 'left top' : 'left bottom',
+                  placement === "bottom-start" ? "left top" : "left bottom",
               }}
             >
               <Paper>
@@ -87,7 +91,7 @@ export default function MenuListComposition() {
                     onKeyDown={handleListKeyDown}
                   >
                     <MenuItem onClick={handleClose}>Profile</MenuItem>
-                    <MenuItem onClick={handleClose}>Logout</MenuItem>
+                    <MenuItem onClick={logout}>Logout</MenuItem>
                   </MenuList>
                 </ClickAwayListener>
               </Paper>
