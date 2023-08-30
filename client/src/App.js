ced1221407ca7c7860d640b0a06e1daa60f69164
Alexander Gorshkov <alexgorshkov0290@gmail.com>
2023-08-30 17:49:25 -0400

    Implemented routes for different pages. Separated the NavBar component. Added a temp solution for logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
 //import logo from './logo.svg';
 import "./App.css";
 import Home from "./components/pages/Home";
+import Dashboard from "./components/pages/Dashboard";
+import Admin from "./components/pages/Admin";
 import React, { useState } from "react";
-import LoginSignupForm from "./components/pages/LoginSignup"
+import LoginSignupForm from "./components/pages/LoginSignup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./elements/NavBar";
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem("jwt"));
   // console.log("loggedIn", loggedIn);
+
   return (
     <div>
       {loggedIn && (
         <div>
-          <Home />
+          <Router>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/admin" element={<Admin />} />
+            </Routes>
+          </Router>
         </div>
       )}
       {!loggedIn && <LoginSignupForm changeLoginState={setLoggedIn} />}
---
+++ client/src/elements/NavBar/index.js
@@ -0,0 +1,93 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import MenuListElement from "../../elements/MenuListElement";
+import { useNavigate, useLocation } from "react-router-dom";
+import { common } from "@mui/material/colors";
+import { styled } from "@mui/material/styles";
+
+const ColorButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.primary.main,
+  fontWeight: "bold",
+  backgroundColor: common.white,
+  "&:hover": {
+    backgroundColor: common.white,
+  },
+  marginRight: "20px",
+}));
+
+// const drawerWidth = 240;
+
+export default function NavBar() {
+  const location = useLocation();
+  let position = "sticky";
+  let drawerWidth = 0;
+  if (location.pathname === "/") {
+    position = "fixed";
+    drawerWidth = 240;
+  }
+
+  const navigate = useNavigate();
+  const handleHomePageClick = () => {
+    navigate("/");
+  };
+  const handleDashboardPageClick = () => {
+    navigate("/dashboard");
+  };
+  const handleAdminPageClick = () => {
+    navigate("/admin");
+  };
+  const showFilesButton = location.pathname !== "/";
+  const showDashboardButton = location.pathname !== "/dashboard";
+  const showAdminButton = location.pathname !== "/admin";
+
+  // console.log("position: ", position);
+  // console.log("drawerWidth: ", drawerWidth);
+  return (
+    <AppBar
+      position={position}
+      sx={{
+        width: `calc(100% - ${drawerWidth}px)`,
+        ml: `${drawerWidth}px`,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Toolbar>
+        <Typography variant="h6" noWrap component="div">
+        {showFilesButton && (
+        <ColorButton
+            variant="contained"
+            size="large"
+            onClick={handleHomePageClick}
+          >
+            Files
+          </ColorButton>
+          )}
+          {showDashboardButton && (
+          <ColorButton
+            variant="contained"
+            size="large"
+            onClick={handleDashboardPageClick}
+          >
+            Dashboard
+          </ColorButton>
+          )}
+          {showAdminButton && (
+          <ColorButton
+            variant="contained"
+            size="large"
+            onClick={handleAdminPageClick}
+          >
+            Admin
+          </ColorButton>
+          )}
+        </Typography>
+      </Toolbar>
+      <MenuListElement />
+    </AppBar>
+  );
+}
